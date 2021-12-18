f16b7c1c8953294a3083bf60485da98b1a2f9bd1
bigpond4932 <91193268+bigpond4932@users.noreply.github.com>
2021-12-18 11:44:50 +0900

    12 월 17일 배운 거

---
+++ 공부내역/2021.12.17.py
@@ -0,0 +1,43 @@
+# 오늘 배울 것은 배열1
+
+# 배열은 고정된 크기만 가진다. 고정된 크기를 가진다.
+# 속도가 빨르다. 데이터를 빨리 가져온다.
+
+# 리스트 반대로 전체를 할당하지 않으니 100 이상으로 늘릴 수 있다.
+# 정수는 32개의 비트
+# 리스트는 위치에 대한 값까지 가지고 있기 때문에 공간을 많이 먹는다.
+# 리스트는 remove에 n번 움직인다.
+
+
+# list= input().split()
+
+# x= int(list[0])
+# y= int(list[1])
+# print(type(x))
+
+# if (x > 0 and y > 0): 
+#     print(1)
+# if (x > 0 and y < 0 ):
+#     print(4)
+# if (x < 0 and y > 0): 
+#     print(2)
+# if (x < 0 and y < 0): 
+#     print(4)
+
+# type) string int charactor boolean double floot(실수 표현) 기본 자료형
+# list 같은 거는 똑똑한 사람들이 만든 객체
+
+
+list = input().split()
+
+a = int(list[0])
+b = int(list[1])
+
+# print(a)
+# print(b)
+
+print(a*b0)
+print(a*b1)
+print(a*b2)
+
+#문자열 =배열 char(문자) / 문자의 배열 => string
