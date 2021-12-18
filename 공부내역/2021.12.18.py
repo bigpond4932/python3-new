afcda9e9aee26ac69e0b041198b529dfc2f92d0b
bigpond4932 <91193268+bigpond4932@users.noreply.github.com>
2021-12-18 13:43:39 +0900

    12/18 배운것

---
+++ 공부내역/2021.12.18.py
@@ -0,0 +1,30 @@
+# 오늘은 반복문=> 같은 작업을 반복해주는 친구
+
+
+
+# 근본 문자( int char float(소수점) )  =>자료형
+# Long, string(char의 집합)  double
+
+
+# list array  자료형과 문법으로 만든 거
+# 
+# for if 
+
+# =를 쓰기 위해서는 타입이 같아야 함.
+
+# list.insert() #arguemenet, parameter, 인자
+# #list remove/당겨진다 remove/ insert or append(끝에)/ 리스트 변환
+
+# if for문 평생함께 한다.
+
+
+# 별찍기
+# a = int(input())
+# for i in range(1,a+1):
+#     print(i*'*')
+
+if True: # True는 boolean 값이다. true 는 1 false 은 0
+    print(1)   
+    if False:
+        print(1)
+
